9c5dca3a982a3a044d46b2d692d44f297fc385b2
joao911 <joaopaulolacerda911@gmail.com>
2025-11-17 16:15:30 -0300

    feat: created folder components in folder layout

--- src/components/Layout/components/Header/index.tsx
+++ src/components/Layout/components/Header/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Link from "next/link";
-import { Button } from "../ui/button";
-import { ActiveLink } from "../ActiveLink";
-import { Logo } from "../Logo";
+import { Button } from "../../../ui/button";
+import { ActiveLink } from "../../../ActiveLink";
+import { Logo } from "../../../Logo";
 
 export const Header: React.FC = () => {
   return (
     <header className="fixed top-0 z-50 w-full text-white border-b border-white/10 ">
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-80">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Logo />
 
