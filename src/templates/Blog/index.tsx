50455bdadb131acec5946641b77f34097266ace9
joao911 <joaopaulolacerda911@gmail.com>
2025-11-23 21:06:06 -0300

    feat: added ssg in blog

--- src/templates/Blog/index.tsx
+++ src/templates/Blog/index.tsx
@@ -1,11 +1,15 @@
 import { useRouter } from "next/router";
+import { Inbox } from "lucide-react";
 
 import { Search } from "@/components/Search";
 import { PostCard } from "./components/PostCard";
+import { allPosts, Post } from "contentlayer/generated";
 import { PostGridCard } from "./components/PostGridCard";
-import { allPosts } from "contentlayer/generated";
-import { Inbox } from "lucide-react";
-export const Blog = () => {
+
+export type BlogProps = {
+  posts: Post[];
+};
+export const Blog = ({ posts }: BlogProps) => {
   const router = useRouter();
   const query = router.query.q as string;
   const pageTitle = query
